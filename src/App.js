75f1d49408a8ef1395b4da6aae848fa421e357ef
felpgoes <felipe.goess@hotmail.com>
2020-01-18 12:06:10 -0300

    update main page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import api from './services/api'
+
+import "./global.css";
+import "./app.css";
+import "./Sidebar.css";
+import "./main.css";
+
+import DevItem from  './components/DevItem'
+import DevForm from  './components/DevForm'
+
 
 function App() {
+  const [devs, setDevs] = useState([])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const res = await api.get('/devs');
+      setDevs(res.data);
+    }
+    loadDevs();
+  }, [])
+
+  async function handleAddDev(data) {
+    const res = await api.post('/devs', data)
+    setDevs([...devs, res.data])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map( dev => (
+            <DevItem key={dev._id} dev={dev} />     
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
